d0b354e621cfbee7d3440ac5bc4a7bf7c7e53f30
Muhammadalif1213 <muhammadalif1213@gmail.com>
2024-11-21 22:48:19 +0700

    menambahkan jenis kelamin (laki-laki dan wanita)

---
+++ app/src/main/java/com/example/pertemuan7/model/JenisKelamin.kt
@@ -0,0 +1,10 @@
+package com.example.pertemuan7.model
+
+import com.example.pertemuan7.R
+
+object JenisKelamin {
+    val listJK = listOf(
+        R.string.laki,
+        R.string.wanita
+    )
+}
