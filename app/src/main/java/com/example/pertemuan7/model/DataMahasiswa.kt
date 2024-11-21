32fc07bce6b8b1274e3916824c5c1f79e235a304
Muhammadalif1213 <muhammadalif1213@gmail.com>
2024-11-21 22:46:25 +0700

    menambahkan variabel di model DataMahasiswa

---
+++ app/src/main/java/com/example/pertemuan7/model/DataMahasiswa.kt
@@ -0,0 +1,11 @@
+package com.example.pertemuan7.model
+
+data class DataMahasiswa(
+    val nama:String="",
+    val NIM:String="",
+    val gender:String="",
+    val alamat:String="",
+    val email:String="",
+    val noHp:String="",
+
+)
